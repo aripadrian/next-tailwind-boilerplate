9cfdbd1acfaf4fa3bf07e6bc5c0748507134003e
aripadrian <aripadrian01@gmail.com>
2021-12-29 11:45:46 +0700

    first init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import 'tailwindcss/tailwind.css'
+import { Auth } from '@supabase/ui'
+import { supabase } from '../utils/initSupabase'
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main>
+      <Auth.UserContextProvider supabaseClient={supabase}>
+        <Component {...pageProps} />
+      </Auth.UserContextProvider>
+    </main>
+  )
+}
